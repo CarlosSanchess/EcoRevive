f289d955428bfc9b3a14bf0d9ff3a98ffc2b9d5d
Cruddle <up202107694@up.pt>
2024-05-04 18:33:14 +0100

    Deleting Users, as an admin function implemented.

---
+++ lib/API/API.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class API {
+
+  static const String baseUrl = 'https://esof-api.onrender.com';
+
+  Future<void> banUser(String uid) async {
+    final uri = Uri.parse("$baseUrl/ban");
+
+    try {
+      final response = await http.post(
+        uri,
+        body: jsonEncode(<String, String>{
+          'uid': uid,
+        }),
+      );
+      if (response.statusCode == 200) {
+        return json.decode(response.body);
+      } else {
+        throw Exception('Failed to ban user: ${response.statusCode}');
+      }
+    } catch (error) {
+      throw Exception('Failed to ban user: $error');
+    }
+  }
+
+  Future<void> sendMessage(String fcmToken) async{
+    final uri = Uri.parse("$baseUrl/send");
+
+    try{
+      final response = await http.post(
+        uri,
+        body: jsonEncode(<String, String>{
+          'fcmToken': fcmToken,
+        }),
+      );
+    if (response.statusCode == 200) {
+        return json.decode(response.body);
+    } else {
+        throw Exception('Failed to ban user: ${response.statusCode}');
+      }
+    } catch (error) {
+      throw Exception('Failed to ban user: $error');
+     }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:provider/provider.dart';
 import 'package:register/Auth/Auth.dart';
 import 'package:register/Pages/Home.dart';
@@ -8,11 +9,29 @@ import 'package:register/Pages/ModeratorHome.dart';
 import 'package:register/Pages/theme_provider.dart';
 import 'package:register/firebase_options.dart';
 import 'package:register/Controllers/NotificationController.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:register/API/API.dart';
+
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async
+{
+  await Firebase.initializeApp();
+}
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseMessaging.instance.getToken();
+  //await PushNotificationService().initialise();
+  //FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    // Access specific fields if needed, e.g., message.data['field_name']
+    print(message.data);
+  });
+
+
   runApp(
     ChangeNotifierProvider(
       create: (_) => ThemeProvider(),
