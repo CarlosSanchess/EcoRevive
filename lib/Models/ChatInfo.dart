350f15f0d28208ce8f66bebdf771898c56fd9510
Cruddle <up202107694@up.pt>
2024-05-22 18:08:51 +0100

    Added chat moderation

--- lib/Models/ChatInfo.dart
+++ lib/Models/ChatInfo.dart
@@ -1,9 +1,11 @@
 class ChatInfo{
+  final String chatId;
   final String nameID1;
   final String nameID2;
   final String nameProduct;
 
   ChatInfo({
+    required this.chatId,
     required this.nameID1,
     required this.nameID2,
     required this.nameProduct
